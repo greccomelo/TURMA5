1fb6f213c8bb43b7f01613ef861f74e5da989d2f
Bruno Grecco <88808730+greccomelo@users.noreply.github.com>
2022-11-06 16:49:19 -0300

    ER4

--- UC14/CHAPTER/Contexts/ChapterContext.cs
+++ UC14/CHAPTER/Contexts/ChapterContext.cs
@@ -3,27 +3,28 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Chapter.Contexts
 {
-    public class ChapterContext: DbContext
+    public class ChapterContext : DbContext
     {
-        public ChapterContext()
-        { 
-        }
-        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options)
-        { 
-        }
-         // vamos utilizar este metodo para configurar  o banco de dados
-         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public ChapterContext() { }
+
+        public ChapterContext(DbContextOptions<ChapterContext> options) : base(options) { }
+
+
+        // vamos utilizar esse método para configurar o banco de dados
+        protected override void
+        OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                //cada provedor tem sua sintaxe para especificações
-                optionsBuilder.UseSqlServer("Data Source = JOBS; initial catalog = Chapter; Integrated Security = true");
+                // cada provedor tem sua sintaxe para especificação
+                optionsBuilder.UseSqlServer("Data Source = CYBERNOTE-13; initial catalog = Chapter; Integrated Security = true");
+                //optionsBuilder.UseSqlServer("Data Source = CYBERNOTE-03-1\\SQLEXPRESS; initial catalog = Chapter; User ID = sa; Password = 123");
+
             }
-
         }
-        //dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e exclusão.
-        public DbSet<Livro> Livros { get; set; }
-        public DbSet<Usuario> Usuarios { get; set; }
+        // dbset representa as entidades que serão utilizadas nas operações de leitura, criação, atualização e deleção
+        public DbSet<Livro>? Livros { get; set; }
 
+        public DbSet<Usuario> Usuarios { get; set; }
     }
 }
