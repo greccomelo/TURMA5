cfc19e6a9d7c70723b838294f6a09d5c21ab872a
Bruno Grecco <88808730+greccomelo@users.noreply.github.com>
2022-12-06 19:41:14 -0300

    06/12/2022

---
+++ UC14/projeto_teste/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace projeto_teste
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ UC14/projeto_teste/controller/loginControllerTeste.cs
@@ -0,0 +1,38 @@
+﻿using Chapter.Controllers;
+using Chapter.Interfaces;
+using Chapter.Models;
+using Chapter.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace projeto_teste.controller
+{
+    public class loginControllerTeste
+    {
+        [Fact]
+        public void LoginController_Retornar_Usuario_Invalido()
+        {
+            //arrange
+            var fakeRepository = new Mock<IUsuarioRepository>();
+            fakeRepository.Setup(x => x.Login(It.IsAny<string>(), It.IsAny<string>())).Returns((Usuario)null);
+
+            LoginViewModel dadosLogin = new LoginViewModel();
+            dadosLogin.Email = "email@email.com";
+            dadosLogin.Senha = "1234";
+
+            var controller = new LoginController(fakeRepository.Object);
+
+            //act
+            var resultado = controller.Login(dadosLogin);
+
+            //assert
+            Assert.IsType<UnauthorizedObjectResult>(resultado);
+        }
+    }
+}
