17de96bdb80e3c3e6f3ef4a937a2ecc0e3423f97
Bruno Grecco <88808730+greccomelo@users.noreply.github.com>
2022-12-06 19:19:20 -0300

    Add files via upload

---
+++ UC15/TestUnit/UnitTest1.cs
@@ -0,0 +1,37 @@
+using Projeto_teste;
+using Xunit;
+
+namespace TestUnit
+{
+    public class TesteCalculadoraXUnit
+    {
+        [Fact]
+        public void TesteSomarDoisNumeros()
+        {
+            //Arrange - Preparação
+            double pNum = 1;
+            double sNum = 1;
+            double tNum = 2;
+
+            //Act - Ação
+            var resultado = Calculadora.Somar(pNum, sNum);
+
+            //Assert - Verificação
+            Assert.Equal(tNum, resultado);
+        }
+
+
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(2, 2, 4)]
+        [InlineData(1, 2, 2)]
+        public void TesteSomarDoisNumerosLista(double pNum, double sNum, double tNum)
+        {
+            //Act - Ação
+            var resultado = Calculadora.Somar(pNum, sNum);
+
+            //Assert - Verificação
+            Assert.Equal(tNum, resultado);
+        }
+    }
+}
